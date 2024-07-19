f6b5f86384ca02ab390d106b5186d9ea0d6067b1
Nemanja <nemanja@rainforestapp.com>
2024-07-20 00:52:05 +0900

    Change starter event to make testing easier
    - Make Tyrogue a starter instead of Cyndaquil
    - Make getting a starter also give you the dex

--- maps/ElmsLab.asm
+++ maps/ElmsLab.asm
@@ -160,8 +160,8 @@ CyndaquilPokeBallScript:
 	iftrue LookAtElmPokeBallScript
 	turnobject ELMSLAB_ELM, DOWN
 	refreshscreen
-	pokepic CYNDAQUIL
-	cry CYNDAQUIL
+	pokepic TYROGUE
+	cry TYROGUE
 	waitbutton
 	closepokepic
 	opentext
@@ -173,12 +173,13 @@ CyndaquilPokeBallScript:
 	writetext ChoseStarterText
 	promptbutton
 	waitsfx
-	getmonname STRING_BUFFER_3, CYNDAQUIL
+	setflag ENGINE_POKEDEX
+	getmonname STRING_BUFFER_3, TYROGUE
 	writetext ReceivedStarterText
 	playsound SFX_CAUGHT_MON
 	waitsfx
 	promptbutton
-	givepoke CYNDAQUIL, 5, BERRY
+	givepoke TYROGUE, 5, BERRY
 	closetext
 	readvar VAR_FACING
 	ifequal RIGHT, ElmDirectionsScript
