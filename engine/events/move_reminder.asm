0c17a28778615389997c694f00653f4d0d275ef2
Craig Livingstone <2470470l@student.gla.ac.uk>
2024-08-05 12:33:38 +0100

    Fixed move reminder ghost/dark text

--- engine/events/move_reminder.asm
+++ engine/events/move_reminder.asm
@@ -431,7 +431,7 @@ ENDC
 	db "RCK@"
 	db "BRD@"
 	db "BUG@"
-	db "DRK@"
+	db "GHT@"
 	db "STL@"
 	db "NRM@"
 	db "NRM@"
@@ -450,7 +450,7 @@ ENDC
 	db "PSY@"
 	db "ICE@"
 	db "DRG@"
-	db "GHT@"
+	db "DRK@"
 	db "FRY@"
 
 .PrintMoveDesc
